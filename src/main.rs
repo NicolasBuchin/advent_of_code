c691ecceb0a4efe7f36844d73de17618c641e4f5
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-02 21:52:05 +0100

    ok 3

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,40 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    ops::Sub,
 };
 
 fn main() {
-    let (mut left_list, mut right_list) = get_list_from_file("input.txt");
-
-    left_list.sort();
-    right_list.sort();
-
-    let mut sim = 0;
-
-    left_list.iter().for_each(|e| {
-        let s = similarity(*e, &right_list);
-        println!("similarity for {} = {}", e, s);
-        sim += s;
-    });
-    println!("total similarity = {} ", sim);
-}
-
-fn get_list_from_file(path: &str) -> (Vec<i32>, Vec<i32>) {
-    let file = File::open(path).unwrap();
+    let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let mut left_list = Vec::new();
-    let mut right_list = Vec::new();
+    let mut safe_count = 0;
 
     reader.lines().map_while(Result::ok).for_each(|line| {
-        let splited_line: Vec<_> = line.split("   ").collect();
-        left_list.push(splited_line[0].parse().unwrap());
-        right_list.push(splited_line[1].parse().unwrap());
+        let report: Vec<_> = line.split(' ').collect();
+        let report: Vec<i32> = report.iter().map(|s| s.parse().unwrap()).collect();
+        let safe = is_safe(&report);
+        if safe {
+            safe_count += 1;
+        }
+        println!("report {:?} is safe ? {}", report, safe);
     });
 
-    (left_list, right_list)
+    println!("total amount of safe reports = {} ", safe_count);
 }
 
-fn similarity(e: i32, right_list: &[i32]) -> i32 {
-    e.wrapping_mul(right_list.iter().filter(|&x| *x == e).count() as i32)
+fn is_safe(report: &[i32]) -> bool {
+    let ascending = report[0] < report[1];
+
+    let mut prev = report[0];
+    let mut res = true;
+
+    report[1..].iter().for_each(|&x| {
+        if (ascending && (x <= prev)) || (!ascending && (x >= prev)) || (x.sub(prev).abs() > 3) {
+            res = false;
+        }
+        prev = x;
+    });
+
+    res
 }
