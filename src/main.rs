d0ed37092c5b24896acdc6ffe3359d00e6bdce49
Nicolas Buchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-03 13:53:36 +0100

    ok 6th star

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-    ops::Mul,
-};
-
 use regex::Regex;
+use std::fs::{self};
 
 fn main() {
-    let file = File::open("input.txt").unwrap();
-    let reader = BufReader::new(file);
-
     let regex = Regex::new(r"mul[(]([0-9]+),([0-9]+)[)]").unwrap();
 
-    let mul_total: i32 = reader.lines().flatten().map(|line| {
-        regex.captures_iter(&line).map(|c| {let (_, [left, right]) = c.extract();
-            left.parse::<i32>().unwrap().mul(right.parse::<i32>().unwrap())
-        }).sum::<i32>()
-    }).sum();
+    let mul_total: i32 = fs::read_to_string("input.txt")
+        .unwrap()
+        .split("do()")
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|&s| s.split("don't()").collect::<Vec<&str>>()[0])
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|&s| {
+            regex
+                .captures_iter(s)
+                .map(|c| {
+                    let (_, [left, right]) = c.extract();
+                    left.parse::<i32>().unwrap() * right.parse::<i32>().unwrap()
+                })
+                .sum::<i32>()
+        })
+        .sum::<i32>();
 
     println!("result of all valid muls = {} ", mul_total);
 }
