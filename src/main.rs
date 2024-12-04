aaf0d2a2e7b2d7b01765ca3b9b8e6faa017d1699
Nicolas Buchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-04 13:03:21 +0100

    benchmarked + opimized 8th star

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,41 @@ use std::time::Instant;
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
-    let now = Instant::now();
+    let t = 100_000;
+    let mut avg = 0;
 
+    (0..t).for_each(|_| {
+        let now = Instant::now();
+
+        let count = count_x(&input);
+
+        avg += now.elapsed().as_nanos();
+        assert_eq!(count, 1831);
+    });
+
+    avg /= t;
+
+    println!("avg elapsed time: {:.2?}ns", avg);
+}
+
+fn count_x(input: &str) -> usize {
     let puzzle = input.as_bytes();
     let line_length = input.lines().next().unwrap().len() + 1;
 
-    let count = puzzle
+    puzzle[line_length..puzzle.len() - line_length]
         .iter()
         .enumerate()
-        .filter(|(i, &c)| c == b'A' && check_x(*i, puzzle, line_length))
-        .count();
-
-    println!("Elapsed: {:.2?}", now.elapsed());
-
-    println!("ok: {}", count == 1831);
+        .filter(|(i, &c)| {
+            c == b'A'
+                && (i.rem_euclid(line_length) != 0 || i.rem_euclid(line_length) != 1)
+                && check_x(*i + line_length, puzzle, line_length)
+        })
+        .count()
 }
 
 fn check_x(i: usize, puzzle: &[u8], line_length: usize) -> bool {
     i + line_length + 1 < puzzle.len()
         && i > line_length + 1
-        && ((puzzle[i + line_length + 1] == b'S' && puzzle[i - line_length - 1] == b'M')
-            || (puzzle[i + line_length + 1] == b'M' && puzzle[i - line_length - 1] == b'S'))
-        && ((puzzle[i + line_length - 1] == b'S' && puzzle[i - line_length + 1] == b'M')
-            || (puzzle[i + line_length - 1] == b'M' && puzzle[i - line_length + 1] == b'S'))
+        && puzzle[i + line_length - 1] + puzzle[i - line_length + 1] == 160
+        && puzzle[i + line_length + 1] + puzzle[i - line_length - 1] == 160
 }
