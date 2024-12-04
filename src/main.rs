a7d15004ada4e60d22d958631373f66418891672
Nicolas Buchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-04 10:31:28 +0100

    ok 7

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::time::Instant;
 fn main() {
     let now = Instant::now();
 
+    let mut count = 0;
+
     let puzzle = BufReader::new(File::open("input.txt").unwrap())
         .lines()
         .map_while(Result::ok)
@@ -13,7 +15,30 @@ fn main() {
     puzzle.iter().enumerate().for_each(|(y, l)| {
         l.chars().enumerate().for_each(|(x, c)| {
             if c == 'X' {
-                check_horizontal(x, y, &puzzle);
+                if check_horizontal1(x, y, &puzzle) {
+                    count += 1;
+                }
+                if check_horizontal2(x, y, &puzzle) {
+                    count += 1;
+                }
+                if check_vertical1(x, y, &puzzle) {
+                    count += 1;
+                }
+                if check_vertical2(x, y, &puzzle) {
+                    count += 1;
+                }
+                if check_diagonal1(x, y, &puzzle) {
+                    count += 1;
+                }
+                if check_diagonal2(x, y, &puzzle) {
+                    count += 1;
+                }
+                if check_diagonal3(x, y, &puzzle) {
+                    count += 1;
+                }
+                if check_diagonal4(x, y, &puzzle) {
+                    count += 1;
+                }
             }
         })
     });
@@ -21,16 +46,58 @@ fn main() {
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
-    puzzle.iter().for_each(|l| println!("{:?}", l));
+    println!("count: {}", count);
 }
 
-fn check_horizontal(x: usize, y: usize, puzzle: &[String]) -> bool {
-    if puzzle[y].chars().nth(x + 3) != Some('S') {
-        println!("ok -> {} {}", x, y);
-        println!("{:?}", &puzzle[y][x..x + 4]);
-    }
-    if x >= 3 && puzzle[y].chars().nth(x - 3) != Some('S') {
-        println!("ok <- {} {}", x, y);
-    }
-    true
+fn check_horizontal1(x: usize, y: usize, puzzle: &[String]) -> bool {
+    x + 3 < puzzle[y].len()
+        && puzzle[y].chars().nth(x + 3) == Some('S')
+        && puzzle[y].chars().nth(x + 2) == Some('A')
+        && puzzle[y].chars().nth(x + 1) == Some('M')
+}
+fn check_horizontal2(x: usize, y: usize, puzzle: &[String]) -> bool {
+    x >= 3
+        && puzzle[y].chars().nth(x - 3) == Some('S')
+        && puzzle[y].chars().nth(x - 2) == Some('A')
+        && puzzle[y].chars().nth(x - 1) == Some('M')
+}
+fn check_vertical1(x: usize, y: usize, puzzle: &[String]) -> bool {
+    y + 3 < puzzle.len()
+        && puzzle[y + 3].chars().nth(x) == Some('S')
+        && puzzle[y + 2].chars().nth(x) == Some('A')
+        && puzzle[y + 1].chars().nth(x) == Some('M')
+}
+fn check_vertical2(x: usize, y: usize, puzzle: &[String]) -> bool {
+    y >= 3
+        && puzzle[y - 3].chars().nth(x) == Some('S')
+        && puzzle[y - 2].chars().nth(x) == Some('A')
+        && puzzle[y - 1].chars().nth(x) == Some('M')
+}
+fn check_diagonal1(x: usize, y: usize, puzzle: &[String]) -> bool {
+    y + 3 < puzzle.len()
+        && x + 3 < puzzle[y].len()
+        && puzzle[y + 3].chars().nth(x + 3) == Some('S')
+        && puzzle[y + 2].chars().nth(x + 2) == Some('A')
+        && puzzle[y + 1].chars().nth(x + 1) == Some('M')
+}
+fn check_diagonal2(x: usize, y: usize, puzzle: &[String]) -> bool {
+    y + 3 < puzzle.len()
+        && x >= 3
+        && puzzle[y + 3].chars().nth(x - 3) == Some('S')
+        && puzzle[y + 2].chars().nth(x - 2) == Some('A')
+        && puzzle[y + 1].chars().nth(x - 1) == Some('M')
+}
+fn check_diagonal3(x: usize, y: usize, puzzle: &[String]) -> bool {
+    y >= 3
+        && x + 3 < puzzle[y].len()
+        && puzzle[y - 3].chars().nth(x + 3) == Some('S')
+        && puzzle[y - 2].chars().nth(x + 2) == Some('A')
+        && puzzle[y - 1].chars().nth(x + 1) == Some('M')
+}
+fn check_diagonal4(x: usize, y: usize, puzzle: &[String]) -> bool {
+    y >= 3
+        && x >= 3
+        && puzzle[y - 3].chars().nth(x - 3) == Some('S')
+        && puzzle[y - 2].chars().nth(x - 2) == Some('A')
+        && puzzle[y - 1].chars().nth(x - 1) == Some('M')
 }
