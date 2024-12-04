f297730f9bdae293abc5e3abf945dcf9218e1bda
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-04 09:24:03 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
-use regex::Regex;
-use std::fs::{self};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::time::Instant;
 
 fn main() {
-    let regex = Regex::new(r"mul[(]([0-9]+),([0-9]+)[)]").unwrap();
+    let now = Instant::now();
 
-    let mul_total: i32 = fs::read_to_string("input.txt")
-        .unwrap()
-        .split("do()")
-        .collect::<Vec<_>>()
-        .iter()
-        .map(|&s| s.split("don't()").collect::<Vec<&str>>()[0])
-        .collect::<Vec<_>>()
-        .iter()
-        .map(|&s| {
-            regex
-                .captures_iter(s)
-                .map(|c| {
-                    let (_, [left, right]) = c.extract();
-                    left.parse::<i32>().unwrap() * right.parse::<i32>().unwrap()
-                })
-                .sum::<i32>()
+    let puzzle = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .map_while(Result::ok)
+        .collect::<Vec<_>>();
+
+    puzzle.iter().enumerate().for_each(|(y, l)| {
+        l.chars().enumerate().for_each(|(x, c)| {
+            if c == 'X' {
+                check_horizontal(x, y, &puzzle);
+            }
         })
-        .sum::<i32>();
+    });
 
-    println!("result of all valid muls = {} ", mul_total);
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    puzzle.iter().for_each(|l| println!("{:?}", l));
+}
+
+fn check_horizontal(x: usize, y: usize, puzzle: &[String]) -> bool {
+    if puzzle[y].chars().nth(x + 3) != Some('S') {
+        println!("ok -> {} {}", x, y);
+        println!("{:?}", &puzzle[y][x..x + 4]);
+    }
+    if x >= 3 && puzzle[y].chars().nth(x - 3) != Some('S') {
+        println!("ok <- {} {}", x, y);
+    }
+    true
 }
