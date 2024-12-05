c5164c27d47bcde699008c8d8f10a661a0f84415
Nicolas Buchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-05 10:47:36 +0100

    ok 9th star

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::{self};
 use std::time::Instant;
 
@@ -28,13 +28,18 @@ enum State {
     Evaluation,
 }
 
-fn print_queue(input: &str) -> usize {
-    let mut order = HashMap::<i32, i32>::new();
+fn print_queue(input: &str) -> i32 {
+    let mut order = HashMap::<i32, HashSet<i32>>::new();
+    let mut passed = HashSet::<i32>::new();
+    let mut passed_list = Vec::new();
+
     let mut state = State::Rules;
     let mut next_state = false;
 
     let mut key: i32 = 0;
     let mut target = Vec::new();
+
+    let mut ok = true;
 
     let mut count = 0;
 
@@ -42,18 +47,23 @@ fn print_queue(input: &str) -> usize {
         State::Rules => {
             if b.is_ascii_digit() {
                 target.push(b);
+                next_state = false;
             } else {
                 match b {
                     b'|' => {
-                        key = target.iter().fold(0i32, |acc, &digit| acc * 10 + (digit - b'0') as i32);
+                        key = target
+                            .iter()
+                            .fold(0i32, |acc, &digit| acc * 10 + (digit - b'0') as i32);
                         target.clear();
                     }
                     b'\n' => {
                         if next_state {
                             state = State::Evaluation
                         } else {
-                            let value = target.iter().fold(0i32, |acc, &digit| acc * 10 + (digit - b'0') as i32);
-                            order.insert(key, value);
+                            let value = target
+                                .iter()
+                                .fold(0i32, |acc, &digit| acc * 10 + (digit - b'0') as i32);
+                            order.entry(key).or_insert_with(HashSet::new).insert(value);
                             target.clear();
                             next_state = true;
                         }
@@ -64,14 +74,56 @@ fn print_queue(input: &str) -> usize {
         }
         State::Evaluation => {
             if b.is_ascii_digit() {
-                target.push(b);
+                if ok {
+                    target.push(b);
+                }
             } else {
                 match b {
                     b',' => {
-                        ();
+                        if ok {
+                            key = target
+                                .iter()
+                                .fold(0i32, |acc, &digit| acc * 10 + (digit - b'0') as i32);
+                            target.clear();
+
+                            if let Some(values) = order.get(&key) {
+                                for v in passed_list.iter() {
+                                    if values.contains(&v) {
+                                        ok = false;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            passed.insert(key);
+                            passed_list.push(key);
+                        }
                     }
                     b'\n' => {
-                        count += 1;
+                        if ok {
+                            key = target
+                                .iter()
+                                .fold(0i32, |acc, &digit| acc * 10 + (digit - b'0') as i32);
+                            target.clear();
+
+                            if let Some(values) = order.get(&key) {
+                                for v in passed_list.iter() {
+                                    if values.contains(&v) {
+                                        ok = false;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if ok {
+                                let middle = passed_list[passed_list.len().div_ceil(2)];
+                                count += middle;
+                            }
+                        }
+
+                        passed.clear();
+                        passed_list.clear();
+                        ok = true;
                     }
                     _ => (),
                 }
