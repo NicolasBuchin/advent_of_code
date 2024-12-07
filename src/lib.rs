7c1c5dc65aae8b32e3fe172513d3e7f1f01b7664
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-07 21:26:12 +0100

    better par 14th star ++

--- src/lib.rs
+++ src/lib.rs
@@ -4,35 +4,41 @@ pub fn bridge_repair_par(input: &str) -> usize {
     input
         .par_lines()
         .map(|l| {
-            let mut test_numbers = [(0usize, 0usize); 100];
-            let mut test_numbers_len = 0;
+            let bytes = l.as_bytes();
+            let mut i = 0;
 
-            let (digits, digits_mul, target) = l
-                .bytes()
-                .scan((0, 1, 0), |state, b| {
-                    let (digits, digits_mul, target) = state;
-                    if b.is_ascii_digit() {
-                        *digits = *digits * 10 + b as usize - 0x30;
-                        *digits_mul *= 10;
-                    } else if b == b':' {
-                        *target = *digits;
-                        *digits = 0;
-                        *digits_mul = 1;
-                    } else if b == b' ' && *digits != 0 {
-                        test_numbers[test_numbers_len] = (*digits_mul, *digits);
+            let target = {
+                let mut t = 0;
+                while bytes[i] != b':' {
+                    t = 10 * t + bytes[i] as usize - 0x30;
+                    i += 1;
+                }
+                t
+            };
+
+            i += 2;
+
+            let (test_numbers, test_numbers_mul, test_numbers_len) = {
+                let mut test_numbers = [0usize; 12];
+                let mut test_numbers_mul = [0usize; 12];
+                let mut test_numbers_len = 0;
+                bytes[i..]
+                    .split(|&b| b == b' ')
+                    .map(|chunk| {
+                        chunk
+                            .iter()
+                            .fold((0, 1), |(d, dm), &b| (d * 10 + (b as usize - 0x30), dm * 10))
+                    })
+                    .enumerate()
+                    .for_each(|(idx, (digits, digits_mul))| {
+                        test_numbers[idx] = digits;
+                        test_numbers_mul[idx] = digits_mul;
                         test_numbers_len += 1;
-                        *digits = 0;
-                        *digits_mul = 1;
-                    }
-                    Some((*digits, *digits_mul, *target))
-                })
-                .last()
-                .unwrap();
+                    });
+                (test_numbers, test_numbers_mul, test_numbers_len)
+            };
 
-            test_numbers[test_numbers_len] = (digits_mul, digits);
-            test_numbers_len += 1;
-
-            if find_solution(&test_numbers, target, test_numbers_len) {
+            if find_solution(&test_numbers, &test_numbers_mul, target, test_numbers_len) {
                 return target;
             }
             0
@@ -40,16 +46,17 @@ pub fn bridge_repair_par(input: &str) -> usize {
         .sum()
 }
 
-fn find_solution(test_numbers: &[(usize, usize)], target: usize, i: usize) -> bool {
+fn find_solution(test_numbers: &[usize], test_numbers_mul: &[usize], target: usize, i: usize) -> bool {
     if target == 0 {
         return true;
     }
     if i == 0 {
         return false;
     }
-    let (digits_mul, digits) = test_numbers[i - 1];
-    (target.rem_euclid(digits) == 0 && find_solution(test_numbers, target.div_euclid(digits), i - 1))
+    let digits = test_numbers[i - 1];
+    let digits_mul = test_numbers_mul[i - 1];
+    (target.rem_euclid(digits) == 0 && find_solution(test_numbers, test_numbers_mul, target.div_euclid(digits), i - 1))
         || (target.rem_euclid(digits_mul) == digits
-            && find_solution(test_numbers, target.div_euclid(digits_mul), i - 1))
-        || (target >= digits && find_solution(test_numbers, target - digits, i - 1))
+            && find_solution(test_numbers, test_numbers_mul, target.div_euclid(digits_mul), i - 1))
+        || (target >= digits && find_solution(test_numbers, test_numbers_mul, target - digits, i - 1))
 }
