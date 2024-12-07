b109a7b3b0fa00d114ab9b93f963844f26ccfdd4
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-07 18:32:56 +0100

    better par 14th star

--- src/lib.rs
+++ src/lib.rs
@@ -1,102 +1,55 @@
 use rayon::{iter::ParallelIterator, str::ParallelString};
 
-pub fn bridge_repair(input: &str) -> usize {
-    let mut test_numbers = [0usize; 60_000];
-    let mut test_numbers_len = 0;
-
-    let mut digits = 0;
-    let mut digits_mul = 1;
-    let mut target = 0;
-
-    let mut solution_count = 0;
-
-    input.bytes().for_each(|b| {
-        if b.is_ascii_digit() {
-            digits = digits * 10 + b as usize - 0x30;
-            digits_mul *= 10;
-        } else if b == b':' {
-            target = digits;
-            digits = 0;
-            digits_mul = 1;
-        } else if b == b' ' {
-            if digits != 0 {
-                if test_numbers_len == 0 {
-                    test_numbers[0] = digits;
-                    test_numbers_len = 1;
-                } else {
-                    (0..test_numbers_len).for_each(|i| {
-                        let x = test_numbers[i];
-                        test_numbers[i] = digits + x;
-                        test_numbers[test_numbers_len] = digits * x;
-                        test_numbers_len += 1;
-                        test_numbers[test_numbers_len] = x * digits_mul + digits;
-                        test_numbers_len += 1;
-                    });
-                }
-            }
-            digits = 0;
-            digits_mul = 1;
-        } else if b == b'\n' {
-            for &n in &test_numbers[0..test_numbers_len] {
-                if n + digits == target || n * digits == target || n * digits_mul + digits == target {
-                    solution_count += target;
-                    break;
-                }
-            }
-            digits = 0;
-            target = 0;
-            test_numbers_len = 0;
-        }
-    });
-
-    solution_count
-}
-
 pub fn bridge_repair_par(input: &str) -> usize {
     input
         .par_lines()
         .map(|l| {
-            let mut test_numbers = [0usize; 60_000];
+            let mut test_numbers = [(0usize, 0usize); 100];
             let mut test_numbers_len = 0;
 
-            let mut digits = 0;
-            let mut digits_mul = 1;
-            let mut target = 0;
-
-            l.bytes().for_each(|b| {
-                if b.is_ascii_digit() {
-                    digits = digits * 10 + b as usize - 0x30;
-                    digits_mul *= 10;
-                } else if b == b':' {
-                    target = digits;
-                    digits = 0;
-                    digits_mul = 1;
-                } else if b == b' ' {
-                    if digits != 0 {
-                        if test_numbers_len == 0 {
-                            test_numbers[0] = digits;
-                            test_numbers_len = 1;
-                        } else {
-                            (0..test_numbers_len).for_each(|i| {
-                                let x = test_numbers[i];
-                                test_numbers[i] = digits + x;
-                                test_numbers[test_numbers_len] = digits * x;
-                                test_numbers_len += 1;
-                                test_numbers[test_numbers_len] = x * digits_mul + digits;
-                                test_numbers_len += 1;
-                            });
-                        }
+            let (digits, digits_mul, target) = l
+                .bytes()
+                .scan((0, 1, 0), |state, b| {
+                    let (digits, digits_mul, target) = state;
+                    if b.is_ascii_digit() {
+                        *digits = *digits * 10 + b as usize - 0x30;
+                        *digits_mul *= 10;
+                    } else if b == b':' {
+                        *target = *digits;
+                        *digits = 0;
+                        *digits_mul = 1;
+                    } else if b == b' ' && *digits != 0 {
+                        test_numbers[test_numbers_len] = (*digits_mul, *digits);
+                        test_numbers_len += 1;
+                        *digits = 0;
+                        *digits_mul = 1;
                     }
-                    digits = 0;
-                    digits_mul = 1;
-                }
-            });
-            for &n in &test_numbers[0..test_numbers_len] {
-                if n + digits == target || n * digits == target || n * digits_mul + digits == target {
-                    return target;
-                }
+                    Some((*digits, *digits_mul, *target))
+                })
+                .last()
+                .unwrap();
+
+            test_numbers[test_numbers_len] = (digits_mul, digits);
+            test_numbers_len += 1;
+
+            if find_solution(&test_numbers, target, test_numbers_len) {
+                return target;
             }
             0
         })
         .sum()
+}
+
+fn find_solution(test_numbers: &[(usize, usize)], target: usize, i: usize) -> bool {
+    if target == 0 {
+        return true;
+    }
+    if i == 0 {
+        return false;
+    }
+    let (digits_mul, digits) = test_numbers[i - 1];
+    (target.rem_euclid(digits) == 0 && find_solution(test_numbers, target.div_euclid(digits), i - 1))
+        || (target.rem_euclid(digits_mul) == digits
+            && find_solution(test_numbers, target.div_euclid(digits_mul), i - 1))
+        || (target >= digits && find_solution(test_numbers, target - digits, i - 1))
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use std::fs::{self};
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let solution = bridge_repair_par(&input);
+    assert_eq!(solution, 254136560217241);
     println!("count: {}", solution);
 }
