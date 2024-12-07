08381c45b9868bc5beb444584b7f2d6254892af2
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-07 12:43:30 +0100

    14th star + parallelized version

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+use rayon::{iter::ParallelIterator, str::ParallelString};
+
 pub fn bridge_repair(input: &str) -> usize {
-    let mut test_numbers = [0usize; 1024];
+    let mut test_numbers = [0usize; 60_000];
     let mut test_numbers_len = 0;
 
     let mut digits = 0;
+    let mut digits_mul = 1;
     let mut target = 0;
 
     let mut solution_count = 0;
@@ -10,9 +13,11 @@ pub fn bridge_repair(input: &str) -> usize {
     input.bytes().for_each(|b| {
         if b.is_ascii_digit() {
             digits = digits * 10 + b as usize - 0x30;
+            digits_mul *= 10;
         } else if b == b':' {
             target = digits;
             digits = 0;
+            digits_mul = 1;
         } else if b == b' ' {
             if digits != 0 {
                 if test_numbers_len == 0 {
@@ -24,13 +29,16 @@ pub fn bridge_repair(input: &str) -> usize {
                         test_numbers[i] = digits + x;
                         test_numbers[test_numbers_len] = digits * x;
                         test_numbers_len += 1;
+                        test_numbers[test_numbers_len] = x * digits_mul + digits;
+                        test_numbers_len += 1;
                     });
                 }
             }
             digits = 0;
+            digits_mul = 1;
         } else if b == b'\n' {
             for &n in &test_numbers[0..test_numbers_len] {
-                if n + digits == target || n * digits == target {
+                if n + digits == target || n * digits == target || n * digits_mul + digits == target {
                     solution_count += target;
                     break;
                 }
@@ -42,4 +50,56 @@ pub fn bridge_repair(input: &str) -> usize {
     });
 
     solution_count
+}
+
+pub fn bridge_repair_par(input: &str) -> usize {
+    input
+        .par_lines()
+        .map(|l| {
+            let mut test_numbers = [0usize; 60_000];
+            let mut test_numbers_len = 0;
+
+            let mut digits = 0;
+            let mut digits_mul = 1;
+            let mut target = 0;
+
+            let mut solution_count = 0;
+
+            l.bytes().for_each(|b| {
+                if b.is_ascii_digit() {
+                    digits = digits * 10 + b as usize - 0x30;
+                    digits_mul *= 10;
+                } else if b == b':' {
+                    target = digits;
+                    digits = 0;
+                    digits_mul = 1;
+                } else if b == b' ' {
+                    if digits != 0 {
+                        if test_numbers_len == 0 {
+                            test_numbers[0] = digits;
+                            test_numbers_len = 1;
+                        } else {
+                            (0..test_numbers_len).for_each(|i| {
+                                let x = test_numbers[i];
+                                test_numbers[i] = digits + x;
+                                test_numbers[test_numbers_len] = digits * x;
+                                test_numbers_len += 1;
+                                test_numbers[test_numbers_len] = x * digits_mul + digits;
+                                test_numbers_len += 1;
+                            });
+                        }
+                    }
+                    digits = 0;
+                    digits_mul = 1;
+                }
+            });
+            for &n in &test_numbers[0..test_numbers_len] {
+                if n + digits == target || n * digits == target || n * digits_mul + digits == target {
+                    solution_count += target;
+                    break;
+                }
+            }
+            solution_count
+        })
+        .sum()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use advent_of_code::bridge_repair;
+use advent_of_code::bridge_repair_par;
 use std::fs::{self};
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    let solution = bridge_repair(&input);
+    let solution = bridge_repair_par(&input);
     println!("count: {}", solution);
 }
