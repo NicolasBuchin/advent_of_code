0af116a4475964c91ed3214e679290b60afcdc59
Nicolas Buchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-10 16:18:49 +0100

    20th star ok :] + opti

--- src/lib.rs
+++ src/lib.rs
@@ -28,63 +28,95 @@ pub fn hoof_it(input: &str) -> usize {
 
     zeroes[0..zeroes_len]
         .par_iter()
-        .map(|i| find_raiting(*i, b'0', line_size, bytes.len(), bytes))
+        .map(|&i| find_raiting(i, line_size, bytes.len(), bytes))
         .sum()
 }
 
-fn find_score(i: usize, line_size: usize, max_length: usize, bytes: &[u8]) -> usize {
-    let mut lookup = HashSet::new();
-    lookup.insert(i);
-
+fn find_trails(i: usize, line_size: usize, max_length: usize, bytes: &[u8]) -> usize {
+    let mut lookup = [0usize; 25];
+    lookup[0] = i;
+    let mut lookup_len = 1;
     let mut score = b'0';
 
     while score != b'9' {
         score += 1;
-        let mut new_lookup = HashSet::new();
-        for i in lookup.iter() {
+        let mut new_lookup = [0usize; 25];
+        let mut new_lookup_len = 0;
+
+        for i in &lookup[0..lookup_len] {
             if *i > line_size && bytes[*i - line_size] == score {
-                new_lookup.insert(*i - line_size);
+                new_lookup[new_lookup_len] = *i - line_size;
+                new_lookup_len += 1;
             }
             if *i < max_length - line_size && bytes[*i + line_size] == score {
-                new_lookup.insert(*i + line_size);
+                new_lookup[new_lookup_len] = *i + line_size;
+                new_lookup_len += 1;
             }
             let rem = i.rem_euclid(line_size);
             if rem > 0 && bytes[*i - 1] == score {
-                new_lookup.insert(*i - 1);
+                new_lookup[new_lookup_len] = *i - 1;
+                new_lookup_len += 1;
             }
             if rem < line_size - 1 && bytes[*i + 1] == score {
-                new_lookup.insert(*i + 1);
+                new_lookup[new_lookup_len] = *i + 1;
+                new_lookup_len += 1;
             }
         }
         lookup = new_lookup;
+        lookup_len = new_lookup_len;
     }
 
-    lookup.len()
+    let mut unique = [0usize; 25];
+    let mut unique_len = 0;
+    for &i in &lookup[0..lookup_len] {
+        let mut not_in = true;
+        for &j in &unique[0..unique_len] {
+            if i == j {
+                not_in = false;
+                break;
+            }
+        }
+        if not_in {
+            unique[unique_len] = i;
+            unique_len += 1;
+        }
+    }
+    unique_len
 }
 
-fn find_raiting(i: usize, score: u8, line_size: usize, max_length: usize, bytes: &[u8]) -> usize {
-    if bytes[i] != score {
-        return 0;
-    }
-    if score == b'9' {
-        return 1;
+fn find_raiting(i: usize, line_size: usize, max_length: usize, bytes: &[u8]) -> usize {
+    let mut lookup = [0usize; 25];
+    lookup[0] = i;
+    let mut lookup_len = 1;
+    let mut score = b'0';
+
+    while score != b'9' {
+        score += 1;
+        let mut new_lookup = [0usize; 25];
+        let mut new_lookup_len = 0;
+
+        for i in &lookup[0..lookup_len] {
+            if *i > line_size && bytes[*i - line_size] == score {
+                new_lookup[new_lookup_len] = *i - line_size;
+                new_lookup_len += 1;
+            }
+            if *i < max_length - line_size && bytes[*i + line_size] == score {
+                new_lookup[new_lookup_len] = *i + line_size;
+                new_lookup_len += 1;
+            }
+            let rem = i.rem_euclid(line_size);
+            if rem > 0 && bytes[*i - 1] == score {
+                new_lookup[new_lookup_len] = *i - 1;
+                new_lookup_len += 1;
+            }
+            if rem < line_size - 1 && bytes[*i + 1] == score {
+                new_lookup[new_lookup_len] = *i + 1;
+                new_lookup_len += 1;
+            }
+        }
+        lookup = new_lookup;
+        lookup_len = new_lookup_len;
     }
 
-    let mut raiting = 0;
-
-    if i > line_size {
-        raiting += find_raiting(i - line_size, score + 1, line_size, max_length, bytes);
-    }
-    if i < max_length - line_size {
-        raiting += find_raiting(i + line_size, score + 1, line_size, max_length, bytes);
-    }
-    let rem = i.rem_euclid(line_size);
-    if rem > 0 {
-        raiting += find_raiting(i - 1, score + 1, line_size, max_length, bytes);
-    }
-    if rem < line_size - 1 {
-        raiting += find_raiting(i + 1, score + 1, line_size, max_length, bytes);
-    }
-
-    raiting
+    lookup_len
 }
