42f7decfda56f6daf933534a26fd1600e812a582
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-12 21:08:42 +0100

    23rd star ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,75 @@
 pub fn garden_groups(input: &str) -> usize {
-    let bytes = input.as_bytes();
+    let mut input_clone = input.to_owned();
+    let bytes = unsafe { input_clone.as_bytes_mut() };
 
-    let mut islands = [0usize; 26];
-    let mut previous_line = [b'/'; 140];
-    let mut current_line = [b'/'; 140];
+    let line_size = {
+        let mut i = 0;
+        while bytes[i] != b'\n' {
+            i += 1;
+        }
+        i + 1
+    };
 
-    let mut i = 0;
-    let mut j = 0;
-    let mut sum = 0;
-
-    while j < bytes.len() {
-        if bytes[j] == b'\n' {
-            previous_line = current_line;
-            current_line = [b'/'; 140];
-            i = 0;
-            j += 1;
+    let mut total = 0;
+    for i in 0..bytes.len() {
+        let value = bytes[i];
+        if value > b'Z' || value == b'\n' {
             continue;
         }
-        let value = bytes[j] - b'A';
-        if previous_line[i] == value && i > 0 && current_line[i - 1] == value {
-            // idk
-        } else if previous_line[i] == value || (i > 0 && current_line[i - 1] == value) {
-            islands[value as usize] += 2;
-        } else {
-            sum += islands[value as usize] * value as usize;
-            println!("{} : {}", char::from(value + b'A'), islands[value as usize]);
-            islands[value as usize] = 4;
+        let (n, w) = garden_area(value, i, bytes, line_size);
+        total += n * w;
+    }
+
+    total
+}
+
+fn garden_area(value: u8, i: usize, bytes: &mut [u8], line_size: usize) -> (usize, usize) {
+    bytes[i] += 27;
+    let (mut num, mut walls) = (1, 0);
+    if i >= line_size {
+        if bytes[i - line_size] == value {
+            let (n, w) = garden_area(value, i - line_size, bytes, line_size);
+            num += n;
+            walls += w;
+        } else if bytes[i - line_size] != bytes[i] {
+            walls += 1;
         }
-        current_line[i] = value;
-        i += 1;
-        j += 1;
+    } else {
+        walls += 1;
     }
-
-    for x in j - i..j {
-        sum += islands[x] * x;
-        println!("{} : {}", char::from(x as u8 + b'A'), islands[x]);
+    if i < bytes.len() - line_size {
+        if bytes[i + line_size] == value {
+            let (n, w) = garden_area(value, i + line_size, bytes, line_size);
+            num += n;
+            walls += w;
+        } else if bytes[i + line_size] != bytes[i] {
+            walls += 1;
+        }
+    } else {
+        walls += 1;
     }
-
-    sum
+    let rem = i.rem_euclid(line_size);
+    if rem > 0 {
+        if bytes[i - 1] == value {
+            let (n, w) = garden_area(value, i - 1, bytes, line_size);
+            num += n;
+            walls += w;
+        } else if bytes[i - 1] != bytes[i] {
+            walls += 1;
+        }
+    } else {
+        walls += 1;
+    }
+    if rem < line_size - 1 {
+        if bytes[i + 1] == value {
+            let (n, w) = garden_area(value, i + 1, bytes, line_size);
+            num += n;
+            walls += w;
+        } else if bytes[i + 1] != bytes[i] {
+            walls += 1;
+        }
+    } else {
+        walls += 1;
+    }
+    (num, walls)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use advent_of_code::garden_groups;
 use std::fs::{self};
 
 fn main() {
-    let input = fs::read_to_string("test.txt").unwrap();
+    let input = fs::read_to_string("input.txt").unwrap();
     let input = input.replace('\r', "");
     let solution = garden_groups(&input);
     assert_eq!(solution, 1930);
