cf61ba5e7072cb80eb30c6a951b8a42f9007e9e2
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-12-13 23:11:49 +0100

    26th star + linear algebra

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Div;
+
 pub fn claw_contraption(input: &str) -> usize {
     let bytes = input.as_bytes();
     let mut score = 0;
@@ -19,6 +21,7 @@ pub fn claw_contraption(input: &str) -> usize {
             prize_x += bytes[i] as usize - 0x30;
             i += 1;
         }
+        prize_x += 10000000000000;
 
         i += 4;
 
@@ -28,24 +31,47 @@ pub fn claw_contraption(input: &str) -> usize {
             prize_y += bytes[i] as usize - 0x30;
             i += 1;
         }
+        prize_y += 10000000000000;
 
         i += 14;
 
-        score += solve(ax, ay, bx, by, prize_x, prize_y);
+        // println!(
+        //     "A: x+{} y+{}\nB: x+{} y+{}\nP: x={} y={}",
+        //     ax, ay, bx, by, prize_x, prize_y
+        // );
+
+        let solution = solve(
+            (ax as f64, ay as f64),
+            (bx as f64, by as f64),
+            (prize_x as f64, prize_y as f64),
+        );
+
+        // println!("   solution {}\n", solution);
+
+        score += solution;
     }
 
     score
 }
 
-fn solve(ax: usize, ay: usize, bx: usize, by: usize, prize_x: usize, prize_y: usize) -> usize {
-    let mut best_score = 0usize;
-    for ac in 0..=100 {
-        for bc in 0..100 {
-            let score = ac * 3 + bc;
-            if ac * ax + bc * bx == prize_x && ac * ay + bc * by == prize_y && (score < best_score || best_score == 0) {
-                best_score = score;
-            }
-        }
+fn solve(a: (f64, f64), b: (f64, f64), p: (f64, f64)) -> usize {
+    let det = (a.0 * b.1) - (b.0 * a.1);
+    // println!("det {} ", det);
+
+    let i = (p.0 * b.1.div(det) - p.1 * b.0.div(det)).round() as usize;
+    let j = (p.1 * a.0.div(det) - p.0 * a.1.div(det)).round() as usize;
+
+    // println!("i: {}   j: {}", i, j);
+
+    let x = i * a.0 as usize + j * b.0 as usize;
+    let y = i * a.1 as usize + j * b.1 as usize;
+
+    // println!("x : {}   y : {}", x, y);
+    // println!("px: {}   py: {}", p.0 as usize, p.1 as usize);
+
+    if x == p.0 as usize && y == p.1 as usize {
+        return i * 3 + j;
     }
-    best_score
+
+    0
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let input = input.replace('\r', "");
     let solution = claw_contraption(&input);
-    assert_eq!(solution, 480);
+    assert_eq!(solution, 90798500745591);
     println!("count: {}", solution);
 }
